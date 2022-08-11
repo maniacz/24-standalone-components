396201b10b4973865951706919509312c22a8bb1
Lukasz Kipka <lukasz.kipka@pb.com>
2022-08-11 09:36:49 +0200

    350. Services & Standalone Components - zakomentowana stara metoda, nie trzeba tego robić tak

--- src/app/shared/analytics.service.ts
+++ src/app/shared/analytics.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({ providedIn: 'root' })
+// @Injectable()
+@Injectable({providedIn: 'root'})
 export class AnalyticsService {
   registerClick() {
     console.log('Clicked!');
